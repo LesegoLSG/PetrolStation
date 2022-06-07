d478abd9a3573a1d143d9378b78d5a258f36d124
LesegoLSG <217057620@student.uj.ac.za>
2022-06-07 17:50:54 -0500

    Added an ArrayList class

---
+++ src/ArrayList.java
@@ -0,0 +1,42 @@
+import java.util.Iterator;
+
+
+public class ArrayList<T> implements List<T>, Iterator<T> {
+
+	@Override
+	public T get(Integer i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void set(Integer i, T e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void add(Integer i, T e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public T remove(Integer i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Integer size() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
