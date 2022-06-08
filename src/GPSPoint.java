1f87a30a0d941f0455e92a849b57638699ef243a
LesegoLSG <217057620@student.uj.ac.za>
2022-06-08 17:54:28 -0500

    Refactored the calculateDistance method

--- src/GPSPoint.java
+++ src/GPSPoint.java
@@ -35,7 +35,7 @@ public class GPSPoint implements Serializable{
 		double s2 = Math.sin(deltaLong/2);
 		double a = Math.pow(s1, 2) + Math.pow(s2, 2) * Math.cos(Math.toRadians(this.latitude)) * Math.cos(Math.toRadians(otherPoint.latitude));
 		double b = 2* Math.atan2(Math.sqrt(a),Math.sqrt(1-a));
-		return MARSRADIUS;
+		return MARSRADIUS*b;
 	}
 	
 	public String toString(){
